5d47186d90ca7b3ec063feb5865138fcc9cc1e73
Your Name <christof-renner@t-online.de>
2022-08-18 22:42:49 +0200

    added Tweet function

--- main.go
+++ main.go
@@ -6,13 +6,27 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
+
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/dghubble/oauth1"
 )
 
 //go:embed output.txt
 var content string
+
+// Twittercreds
+var TwconsumerKey string = ""
+var TwconsumerSecret string = ""
+var TwaccessToken string = ""
+var TwaccessTokenSecret string = ""
+
+// vars
 var outstr string
 var out []byte
 var err error
+var diff string
+var Tweetcontent string
 
 func ClearDir(dir string) error {
 	files, err := filepath.Glob(filepath.Join(dir, "*"))
@@ -33,8 +47,9 @@ func compare() {
 	outstr = string(out)
 
 	if outstr == content {
-
 		fmt.Println("nothing changed")
+	} else {
+		getdiff()
 	}
 
 }
@@ -52,13 +67,50 @@ func callHR() {
 	}
 }
 
-func main() {
+func getdiff() {
+	diff = (strings.Replace(outstr, content, "", 1))
+}
 
-	callHR()
-	compare()
+func removeleftover() {
+
 	rm := ClearDir("./cache/")
 	if rm != nil {
 		fmt.Println(rm)
 	}
 
+}
+
+func Tweetoutput() {
+
+	if diff == "" {
+		Tweetcontent = "No Changes in the last x Days"
+	} else {
+		Tweetcontent = "New Change in Handelsregister:" + "\n" + diff
+	}
+
+	config := oauth1.NewConfig(TwconsumerKey, TwconsumerSecret)
+	token := oauth1.NewToken(TwaccessToken, TwaccessTokenSecret)
+
+	httpClient := config.Client(oauth1.NoContext, token)
+	client := twitter.NewClient(httpClient)
+
+	user, _, err := client.Accounts.VerifyCredentials(&twitter.AccountVerifyParams{})
+	if err != nil {
+		fmt.Printf("err: %v\n", err)
+	}
+	fmt.Printf("Account: @%s (%s)\n", user.ScreenName, user.Name)
+	_, resp, err := client.Statuses.Update(Tweetcontent, nil)
+	if err != nil {
+		fmt.Print(err)
+		fmt.Println(resp)
+	}
+}
+
+func main() {
+
+	callHR()
+	compare()
+	getdiff()
+	removeleftover()
+	Tweetoutput()
 }
